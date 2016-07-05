9847f681d596589907d5e141a50603a580f58328
github username <github email address>
2016-07-05 14:23:45 -0400

    completed

--- Types.playground/Contents.swift
+++ Types.playground/Contents.swift
@@ -1,18 +1,18 @@
-let thirdPlanet = "Earth"
 
-var ninthPlanet: String
+let thirdPlanet = "Earth"
+var s = "my string"
+
+var ninthPlanet : String
 ninthPlanet = "Pluto"
 
-let fifthPlanet: String = "Jupiter"
-// fifthPlanet is a constant of type ______
+let fifthPlanet : String = "Jupiter"
 
-let sixthPlanet: String = "Saturn"
-// sixthPlanet is a constant of type ______
+let sixthPlanet : String = "Saturn"
 
-let seventhPlanet = "Uranus"
-// seventhPlanet is a constant of type ______
+let seventhPlanet : String = "Uranus"
 
 let numberOfCountriesInAfrica = 54
 
 let numberOfMoonsOfSaturn = 62
+
 print("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet)")
